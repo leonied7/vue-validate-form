d82893debf88df2c5b44cad4f45982123fbf7481
Denis <kdnn@mail.ru>
2020-10-30 11:22:21 +0700

    init

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import VuePlugin from 'rollup-plugin-vue';
+import pkg from './package.json';
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/main.js',
+		output: {
+			name: pkg.name,
+			file: pkg.browser,
+			format: 'umd',
+			exports: 'named'
+		},
+		plugins: [
+			resolve(), // so Rollup can find `ms`
+			commonjs(), // so Rollup can convert `ms` to an ES module
+			VuePlugin(),
+			babel({
+				babelHelpers: 'bundled',
+				exclude: ['node_modules/**']
+			})
+		]
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// an array for the `output` option, where we can specify
+	// `file` and `format` for each target)
+	{
+		input: 'src/main.js',
+		external: ['ms'],
+		output: [
+			{ file: pkg.main, format: 'cjs', exports: 'named' },
+			{ file: pkg.module, format: 'es', exports: 'named' }
+		],
+		plugins: [
+			VuePlugin(),
+			babel({
+				babelHelpers: 'bundled',
+				exclude: ['node_modules/**']
+			})
+		]
+	}
+];
