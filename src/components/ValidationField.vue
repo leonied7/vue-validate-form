1dde30482331a2e4669c5ae90ccecb9659f29a41
Denis <kdnn@mail.ru>
2020-10-30 23:51:54 +0700

    feat: update concept

--- src/components/ValidationField.vue
+++ src/components/ValidationField.vue
@@ -28,29 +28,46 @@
         });
         return props;
       },
+      providedDefaultValue() {
+        return this.defaultValues[this.name];
+      },
+      defaultValue() {
+        return this.providedDefaultValue !== undefined ? this.providedDefaultValue : this.value;
+      }
     },
-    inject: ["addField", "removeField", "updateField", "setValue"],
+    inject: ["addField", "removeField", "updateField", "setValue", "defaultValues"],
     watch: {
       rules(rules) {
-        this.updateField(this.name, this.name, rules);
+        this.updateField(this.name, { name:this.name, rules });
       },
       value: {
         immediate: true,
         handler(value, oldValue) {
+          const defaultValue = this.defaultValue;
+          if(oldValue === undefined && defaultValue !== value) {
+            return this.$emit("change", defaultValue);
+          }
           this.setValue(this.name, value);
         },
       },
       name: {
         immediate: true,
         handler(name, oldName) {
-          oldName
-            ? this.updateField(oldName, name, this.rules)
-            : this.addField(name, this.rules);
+          if(oldName === undefined) {
+            this.onInit(name);
+          } else {
+            this.updateField(oldName, { name, rules: this.rules })
+          }
         },
       },
     },
     beforeDestroy() {
       this.removeField(this.name);
+    },
+    methods: {
+      onInit(name) {
+        this.addField({ name, rules: this.rules, defaultValue: this.defaultValue });
+      },
     },
     render() {
       return this.$scopedSlots.default(this.props);
--- src/components/ValidationProvider.vue
+++ src/components/ValidationProvider.vue
@@ -1,5 +1,5 @@
 <script>
-  import isEqual from "lodash.isequal";
+  // import isEqual from "lodash.isequal";
   //TODO: mb need add inner default values
 
   export default {
@@ -15,11 +15,13 @@
         fields: {},
         values: {},
         errors: {},
+        dirtyFields: {},
+        innerDefaultValues: {},
       };
     },
     computed: {
       isDirty() {
-        return !isEqual(this.defaultValues, this.values);
+        return !!Object.keys(this.dirtyFields).length;
       },
     },
     methods: {
@@ -32,25 +34,40 @@
         });
         this.$emit("submit");
       },
-      addField(name, rules) {
+      addField({name, rules, defaultValue}) {
         this.$set(this.fields, name, rules);
+        this.$set(this.innerDefaultValues, name, defaultValue);
+        this.$delete(this.dirtyFields, name);
       },
-      updateField(oldName, name, rules) {
-        console.log("updated");
-        this.$set(this.fields, name, {
-          ...this.fields[oldName],
-          name,
-          ...rules,
-        });
-        if (name !== oldName) {
-          this.removeField(oldName);
-        }
+      updateField(oldName, {name, rules}) {
+        this.$set(this.fields, oldName, rules);
+        this.replaceFieldName(oldName, name);
       },
       removeField(name) {
         this.$delete(this.fields, name);
+        this.$delete(this.innerDefaultValues, name);
+        this.$delete(this.values, name);
+        this.$delete(this.errors, name);
+        this.$set(this.dirtyFields, name, true);
+      },
+      replaceFieldName(from, to) {
+        if(from === to) {
+          return;
+        }
+        this.$set(this.fields, to, this.fields[from]);
+        this.$delete(this.fields, from);
+        this.$set(this.dirtyFields, to, this.dirtyFields[from]);
+        this.$delete(this.dirtyFields, from);
+        this.$set(this.innerDefaultValues, to, this.innerDefaultValues[from]);
+        this.$delete(this.innerDefaultValues, from);
+        this.$set(this.values, to, this.values[from]);
+        this.$delete(this.values, from);
+        this.$set(this.errors, to, this.errors[from]);
+        this.$delete(this.errors, from);
       },
       setValue(name, value) {
         this.$set(this.values, name, value);
+        value === this.innerDefaultValues[name] ? this.$delete(this.dirtyFields, name) : this.$set(this.dirtyFields, name, true);
       },
     },
     provide() {
@@ -59,6 +76,7 @@
         updateField: this.updateField,
         removeField: this.removeField,
         setValue: this.setValue,
+        defaultValues: this.defaultValues,
       };
     },
     render() {
@@ -67,6 +85,9 @@
         values: this.values,
         isDirty: this.isDirty,
         errors: this.errors,
+        innerDefaultValues: this.innerDefaultValues,
+        fields: this.fields,
+        dirtyFields: this.dirtyFields,
       });
     },
   };
