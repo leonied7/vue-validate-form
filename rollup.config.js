f3eb000898d2348279507d1c510753a9fb2f93bd
Denis <kdnn@mail.ru>
2020-12-22 16:17:07 +0700

    fix: remove umd version

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,5 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
 import VuePlugin from 'rollup-plugin-vue';
 import pkg from './package.json';
-
-// browser-friendly UMD build
-function getBrowserConfig(input, outputFile, name) {
-  return {
-    input,
-    output: {
-      name,
-      file: outputFile,
-      format: 'umd',
-      exports: 'named'
-    },
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      VuePlugin(),
-      babel({
-        babelHelpers: 'bundled',
-        exclude: ['node_modules/**']
-      })
-    ]
-  };
-}
 
 // CommonJS (for Node) build.
 function getCommonJSConfig(input, outputFile) {
@@ -47,8 +22,6 @@ function getEsConfig(input, outputFile) {
 }
 
 export default [
-  getBrowserConfig('src/index.js', pkg.browser, 'VueValidateForm'),
-
   getCommonJSConfig('src/index.js', pkg.main),
 
   getEsConfig('src/index.js', pkg.module)
