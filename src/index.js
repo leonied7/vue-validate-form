613814b45adf94e292cd6273b6a87b37d1c23164
Denis <kolosov@huntflow.ru>
2022-10-19 18:45:26 +0700

    feat: export symbols

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ValidationProvider from './components/ValidationProvider.js';
 import ValidationField from './components/ValidationField.js';
 import { register } from './validators.js';
+import * as symbols from './components/symbols';
 
-export { ValidationProvider, ValidationField, register as registerValidator };
+export { ValidationProvider, ValidationField, register as registerValidator, symbols };
