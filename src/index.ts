999521fe5f1afbda30417be3023887aaf40a7856
leonied7 <kdnn@mail.ru>
2023-06-28 15:34:25 +0700

    fix: fix release version
    Release-As: 3.0.0-alpha.4

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import ValidationField from './components/ValidationField.vue';
 import ValidationFieldArray from './components/ValidationFieldArray.vue';
 import ValidationErrors from './components/ValidationErrors.vue';
 import * as symbols from './components/symbols';
-import { get } from './components/helpers';
+import { get, has } from './components/helpers';
 
 export { type ValidationError, type ValidationsErrors } from './types/error';
 export { type Resolver, type ResolverResult } from './types/resolver';
@@ -14,5 +14,6 @@ export {
   ValidationFieldArray,
   ValidationErrors,
   symbols,
-  get
+  get,
+  has
 };
