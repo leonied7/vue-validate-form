e9259ebfc9fccdd24be9269cc410c8351bf55d69
leonied7 <kdnn@mail.ru>
2023-06-28 15:27:52 +0700

    fix: add additional types

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import ValidationErrors from './components/ValidationErrors.vue';
 import * as symbols from './components/symbols';
 import { get } from './components/helpers';
 
+export { type ValidationError, type ValidationsErrors } from './types/error';
+export { type Resolver, type ResolverResult } from './types/resolver';
+
 export {
   ValidationProvider,
   ValidationField,
